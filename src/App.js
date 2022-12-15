2428fa5b278b639d3dcd3c991e926ae5e1b006ed
Michael Blaha <blahami2@gmail.com>
2022-12-15 12:18:46 +0100

    QuickStart finished

--- src/App.js
+++ src/App.js
@@ -1,60 +1,20 @@
-// import logo from "./logo.svg";
 import "./App.css";
+import { useState, Component } from "react";
 
-function MyButton() {
-  const name = "HappyButton";
-
-  function handleClick() {
-    alert("You clicked me!");
+class Welcome extends Component {
+  // object is created with props, that are parsed from '... name="..." ...' tag properties into a { name: "...", ... } object
+  // props are immutable
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
   }
-
-  // handleClick() would call the function and the outcome would be 'onClick={<result>}'. Usage without () passes the function itself
-  return <button onClick={handleClick}>I'm a {name}</button>;
 }
-
-const user = {
-  name: "Hedy Lamarr",
-  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
-};
-
-function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </>
-  );
-}
-
-const products = [
-  { title: "Cabbage", id: 1 },
-  { title: "Garlic", id: 2 },
-  { title: "Apple", id: 3 },
-];
-
-// notice the "key=", list elements should always have a key unique in the given list, react identifies rows with that key; DB ID is often used
-const listItems = products.map((product) => (
-  <li key={product.id}>{product.title}</li>
-));
 
 function App() {
+  // always start component names with a capital letter, React treats lowercases as DOM tags
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <p>Welcome to my app</p>
-      </header> */}
-      <MyButton />
-      <Profile />
-      <ul>{listItems}</ul>
+      <Welcome name="Josef" />
+      <Welcome name="Pepa" />
     </div>
   );
 }
