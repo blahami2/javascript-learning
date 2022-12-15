e7ede3ac190f3e22c512f4d00fd5dea29a7845cc
Michael Blaha <blahami2@gmail.com>
2022-12-15 10:58:30 +0100

    Multiple buttons, formatting, moved to VisualCode

--- playground/like-button.js
+++ playground/like-button.js
@@ -1,26 +1,21 @@
-'use strict';
+"use strict";
 
+// might not work due to CORS (Cross-Origin Request)
 function LikeButton(props) {
   const [liked, setLiked] = React.useState(false);
 
   if (liked) {
-    return 'You liked comment number ' + props.commentID;
+    return "You liked comment number " + props.commentID;
   }
 
-  return React.createElement(
-    'button',
-    { onClick: () => setLiked(true) },
-    'Like'
-  );
+  return <button onClick={() => setLiked(true)}>Like</button>;
 }
 
 // Find all DOM containers, and render Like buttons into them.
 // Note we're using CSS classes instead of IDs so that we can find several nodes.
-document.querySelectorAll('.like-button-root').forEach((rootNode) => {
+document.querySelectorAll(".like-button-root").forEach((rootNode) => {
   const root = ReactDOM.createRoot(rootNode);
   // Read the comment ID from a data-* attribute.
   const commentID = parseInt(rootNode.dataset.commentid, 10);
-  root.render(
-    React.createElement(LikeButton, { commentID: commentID })
-  );
+  root.render(React.createElement(LikeButton, { commentID: commentID }));
 });
