fa54ed64b10f931e5db80771a3d5d8c7bdb20484
Michael Blaha <blahami2@gmail.com>
2022-12-15 11:39:27 +0100

    React basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+
+function MyButton() {
+  const name = "HappyButton";
+
+  function handleClick() {
+    alert("You clicked me!");
+  }
+
+  // handleClick() would call the function and the outcome would be 'onClick={<result>}'. Usage without () passes the function itself
+  return <button onClick={handleClick}>I'm a {name}</button>;
+}
+
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
+  );
+}
+
+const products = [
+  { title: "Cabbage", id: 1 },
+  { title: "Garlic", id: 2 },
+  { title: "Apple", id: 3 },
+];
+
+// notice the "key=", list elements should always have a key unique in the given list, react identifies rows with that key; DB ID is often used
+const listItems = products.map((product) => (
+  <li key={product.id}>{product.title}</li>
+));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        <p>Welcome to my app</p>
+      </header> */}
+      <MyButton />
+      <Profile />
+      <ul>{listItems}</ul>
     </div>
   );
 }
