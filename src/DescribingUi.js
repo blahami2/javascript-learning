8dd5fc06553784f9499eab2a6a7ee4e71779e021
Michael Blaha <blahami2@gmail.com>
2022-12-16 17:09:13 +0100

    Describing the UI

---
+++ src/DescribingUi.js
@@ -0,0 +1,10 @@
+import Gallery from "./components/Gallery.js";
+import { Profile } from "./components/Gallery.js";
+
+export default function App() {
+  return (
+    <div>
+      <Gallery />
+    </div>
+  );
+}
